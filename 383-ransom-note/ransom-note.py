f107b160a2186b9031df43716b78f9f381620388
Yotham <yothamsage@gmail.com>
2023-09-22 14:29:40 -0400

    Time: 104 ms (10.99%) | Memory: 13.4 MB (89.54%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -6,29 +6,17 @@ class Solution(object):
         :rtype: bool
         """
         magCount = {}
-        ranCount = {}
         for c in magazine:
             if c not in magCount.keys():
                 magCount[c] = 1
             else:
                 magCount[c] +=1
         for c in ransomNote:
-            if c not in ranCount.keys():
-                ranCount[c] = 1
+            if c in magCount and magCount[c] >= 1:
+                magCount[c] -= 1
             else:
-                ranCount[c] += 1
-
-
-        for item in magCount.keys():
-            if item in ranCount.keys():
-                if magCount[item] >= ranCount[item]:
-                    ranCount[item] = "Done"
-                else:
-                    ranCount[item] = "Nope"
-        print(ranCount)
-        for item in ranCount:
-            if ranCount[item] != "Done":
                 return False
+            
         return True
         
          
