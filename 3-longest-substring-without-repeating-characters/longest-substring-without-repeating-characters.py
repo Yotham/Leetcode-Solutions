f04daa717ca3f66e42cf7e7107ec8a22a0455c7d
Yotham <yothamsage@gmail.com>
2023-09-23 01:21:18 -0400

    Time: 29 ms (85.22%) | Memory: 14.1 MB (52.54%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -4,20 +4,16 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        if len(s) == 0:
-            return 0
         l = len(s)
+        if l == 0:
+            return 0
         seen = {}
+        start = 0
         maxL = 0
-        startVal = 0 
         for i in range(l):
-            if s[i] in seen and startVal <= seen[s[i]]:
-                startVal = seen[s[i]]+1
+            if s[i] in seen and start <= seen[s[i]]:
+                start = seen[s[i]]+1
             else:
-                maxL = max(maxL,i-startVal+1)
+                maxL = max(maxL,i-start+1)
             seen[s[i]] = i
         return maxL
-
-            
-        
-        
