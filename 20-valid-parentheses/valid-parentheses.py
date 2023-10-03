cce351d9a76e283532423b35657d2dda8f565be0
Yotham <yothamsage@gmail.com>
2023-10-03 13:22:19 -0400

    Time: 9 ms (91.59%) | Memory: 13.4 MB (67.08%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        for char in s:
+            if char in '([{':
+                stack.append(char)
+            else:
+                if not stack or \
+                    (char == ')' and stack[-1] != '(') or \
+                    (char == '}' and stack[-1] != '{') or \
+                    (char == ']' and stack[-1] != '['):
+                    return False
+                stack.pop()
+        return not stack
+
