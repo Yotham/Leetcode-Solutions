6abfe43b0fa88674c7ccae80a394acf49974d4b7
Yotham <yothamsage@gmail.com>
2023-10-05 21:13:50 -0400

    Time: 178 ms (65.41%) | Memory: 14.7 MB (11.13%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -6,14 +6,13 @@ class Solution(object):
         :rtype: int
         """
         left = 0
-        right = len(nums) - 1
+        right = len(nums)-1
         while left <= right:
-            mid = (left+right) // 2
+            mid = (left+right)//2
             if nums[mid] == target:
                 return mid
             elif nums[mid] < target:
                 left = mid + 1
             else:
                 right = mid - 1
-        
         return -1
