22c37d55e1219f45678d28b8f007ab611bbf8ac4
Yotham <yothamsage@gmail.com>
2024-01-01 14:18:52 -0500

    Time: 18 ms (28.68%) | Memory: 13.4 MB (50.19%) - LeetSync

---
+++ 551-student-attendance-record-i/student-attendance-record-i.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def checkRecord(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        if s.count('A') < 2 and 'LLL' not in s:
+            return True
+        return False
