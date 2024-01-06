65f591e22eda80465ee8abbb38f412b92dcca110
Yotham <yothamsage@gmail.com>
2024-01-05 23:01:07 -0500

    Time: 101 ms (69.65%) | Memory: 13.7 MB (97.67%) - LeetSync

--- 1708-design-parking-system/design-parking-system.py
+++ 1708-design-parking-system/design-parking-system.py
@@ -9,7 +9,7 @@ class ParkingSystem(object):
         self.big = big
         self.medium = medium
         self.small = small
-        
+
     def addCar(self, carType):
         """
         :type carType: int
@@ -19,20 +19,15 @@ class ParkingSystem(object):
             if self.big > 0:
                 self.big-=1
                 return True
-            else:
-                return False
         elif carType == 2:
             if self.medium > 0:
                 self.medium-=1
                 return True
-            else:
-                return False
         elif carType == 3:
             if self.small > 0:
                 self.small-=1
                 return True
-            else:
-                return False
+        return False
         
 
 
