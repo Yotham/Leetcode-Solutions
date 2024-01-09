bb1bc0b16eeab0a6bd446b4eb0dd09a6b84e0743
Yotham <yothamsage@gmail.com>
2024-01-09 00:26:11 -0500

    Time: 2123 ms (5.10%) | Memory: 21.1 MB (68.79%) - LeetSync

---
+++ 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution(object):
+    def dfs(self,root,l):
+        if root is not None:
+            l.append(root.val)
+            self.dfs(root.left,l)
+            self.dfs(root.right,l)
+    def findMode(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        l = []
+        self.dfs(root,l)
+        print(l)
+        maxCount = 0
+        for val in l:
+            count = l.count(val)
+            if count > maxCount:
+                maxCount = count
+        print(maxCount)
+        output = []
+        add = [output.append(val) for val in l if l.count(val) == maxCount and val not in output]
+        return output
+            
+
+        
