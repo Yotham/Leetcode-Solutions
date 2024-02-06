6c3d9824efd397cf77a5457456536e4cbe621ef2
Yotham <yothamsage@gmail.com>
2024-02-06 04:53:43 -0500

    Time: 166 ms (64.93%) | Memory: 19.1 MB (87.09%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -4,26 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        total_product = 1
-        zero_count = nums.count(0)
+        res = [1] * len(nums)
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums)-1,-1,-1):
+            res[i] *= postfix
+            postfix *= nums[i]
 
-        # If there are more than one 0, all products will be 0
-        if zero_count > 1:
-            return [0] * len(nums)
-
-        # Compute the product of all non-zero elements
-        for num in nums:
-            if num != 0:
-                total_product *= num
-
-        # Generate the result array
-        result = []
-        for num in nums:
-            if zero_count == 0:  # No zeros in the original array
-                result.append(total_product // num)
-            elif num == 0:       # This element is the only zero in the array
-                result.append(total_product)
-            else:                # This element is non-zero, but there is a zero in the array
-                result.append(0)
-
-        return result
+        return res
