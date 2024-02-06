ecc009ba038099125fa4c39280fe3eab307c70b6
Yotham <yothamsage@gmail.com>
2024-02-06 04:15:10 -0500

    Time: 747 ms (5.00%) | Memory: 15 MB (89.93%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -7,22 +7,11 @@ class Solution(object):
         :rtype: List[int]
         """
         counts = {}
-        maxCount = 0
         for num in set(nums):
             count = nums.count(num)
             counts[num] = count
-            maxCount = max(maxCount,count)
-        out = []
-        while True:
-            if k == 0:
-                break
-            for key in counts.keys():
-                if counts[key] == maxCount:
-                    out.append(key)
-                    k-=1
-            maxCount-=1
-            
-        return out
+        counts = sorted(counts.items(), key = lambda x :x[1],reverse = True)
+        return [item[0] for item in counts[:k]]
                     
         
         
