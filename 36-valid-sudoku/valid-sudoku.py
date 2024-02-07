b575ff64ce8f57689ddfdd3276c9b08dc17eac1f
Yotham <yothamsage@gmail.com>
2024-02-07 02:09:53 -0500

    Time: 58 ms (85.70%) | Memory: 11.8 MB (82.31%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -4,43 +4,22 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: bool
         """
-        for i in range(len(board)):
-            column = []
-            row = []
-            for j in range(len(board)):
-                if board[j][i] != '.':
-                    row.append(board[j][i])
-                if board[i][j] != '.':
-                    column.append(board[i][j])
-            if len(row) != len(set(row)):
-                return False
-            if len(column) != len(set(column)):
-                return False
+        cols = defaultdict(set)
+        rows = defaultdict(set)
+        box = defaultdict(set)
 
-        def subBox(values):
-            final = []
-            for num in values:
-                if num != '.':
-                    final.append(num)
-            if len(final) == len(set(final)):
-                return True
-            return False
-            
-        for i in range(len(board)):
-            for j in range(len(board)):
-                if i == 1 or i == 4 or i == 7:
-                    if j == 1 or j == 4 or j == 7:
-                        values = [board[i][j],board[i][j-1],board[i][j+1],
-                        board[i-1][j],board[i-1][j-1],board[i-1][j+1],
-                        board[i+1][j],board[i+1][j-1],board[i+1][j+1]]
-                        if not subBox(values):
-                            return False
-                else:
+        for r in range(len(board)):
+            for c in range(len(board)):
+                if board[r][c] == '.':
                     continue
-        
-                        
-
-        
+                if (board[r][c] in rows[r]) or (board[r][c] in cols[c]) or (board[r][c] in box[r//3,c//3]):
+                    return False
+                rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
+                box[r//3,c//3].add(board[r][c])
+                
         return True
+        
+                
 
         
