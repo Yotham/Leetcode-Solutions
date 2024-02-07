55595bf23025a4cdc3c1ead16aca4d944d396fca
Yotham <yothamsage@gmail.com>
2024-02-07 01:58:03 -0500

    Time: 67 ms (42.45%) | Memory: 11.9 MB (73.90%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -4,55 +4,20 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: bool
         """
-        for i in range(len(board)):
-            column = []
-            row = []
-            for j in range(len(board)):
-                if board[j][i] != '.':
-                    row.append(board[j][i])
-                if board[i][j] != '.':
-                    column.append(board[i][j])
-            if len(row) != len(set(row)):
-                return False
-            if len(column) != len(set(column)):
-                return False
-
-        def subBox(values):
-            final = []
-            for num in values:
-                if num != '.':
-                    final.append(num)
-            if len(final) == len(set(final)):
-                return True
-            return False
-            
-        for i in range(len(board)):
-            for j in range(len(board)):
-                if i == 1:
-                    if j == 1 or j == 4 or j == 7:
-                        values = [board[i][j],board[i][j-1],board[i][j+1],
-                        board[i-1][j],board[i-1][j-1],board[i-1][j+1],
-                        board[i+1][j],board[i+1][j-1],board[i+1][j+1]]
-                        if not subBox(values):
-                            return False
-                elif i == 4:
-                    if j == 1 or j == 4 or j == 7:
-                        values = [board[i][j],board[i][j-1],board[i][j+1],
-                        board[i-1][j],board[i-1][j-1],board[i-1][j+1],
-                        board[i+1][j],board[i+1][j-1],board[i+1][j+1]]
-                        if not subBox(values):
-                            return False
-                elif i == 7:
-                    if j == 1 or j == 4 or j == 7:
-                        values = [board[i][j],board[i][j-1],board[i][j+1],
-                        board[i-1][j],board[i-1][j-1],board[i-1][j+1],
-                        board[i+1][j],board[i+1][j-1],board[i+1][j+1]]
-                        if not subBox(values):
-                            return False
-                else:
+        rows = defaultdict(list)
+        columns = defaultdict(list)
+        box = defaultdict(list)
+        for r in range(len(board)):
+            for c in range(len(board)):
+                if board[r][c] == '.':
                     continue
-        
-                        
+                else:
+                    if (board[r][c] in rows[r]) or (board[r][c] in columns[c]) or (board[r][c] in box[r//3,c//3]):
+                        return False
+                columns[c].append(board[r][c])
+                rows[r].append(board[r][c])
+                box[r//3,c//3].append(board[r][c])
+                
 
         
         return True
