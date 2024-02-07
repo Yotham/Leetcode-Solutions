0c5ede64bef551c1fc00c02dd336d305fb26364f
Yotham <yothamsage@gmail.com>
2024-02-07 01:58:33 -0500

    Time: 57 ms (88.68%) | Memory: 11.9 MB (73.90%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -4,9 +4,9 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: bool
         """
-        rows = defaultdict(list)
-        columns = defaultdict(list)
-        box = defaultdict(list)
+        rows = defaultdict(set)
+        columns = defaultdict(set)
+        box = defaultdict(set)
         for r in range(len(board)):
             for c in range(len(board)):
                 if board[r][c] == '.':
@@ -14,9 +14,9 @@ class Solution(object):
                 else:
                     if (board[r][c] in rows[r]) or (board[r][c] in columns[c]) or (board[r][c] in box[r//3,c//3]):
                         return False
-                columns[c].append(board[r][c])
-                rows[r].append(board[r][c])
-                box[r//3,c//3].append(board[r][c])
+                columns[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                box[r//3,c//3].add(board[r][c])
                 
 
         
