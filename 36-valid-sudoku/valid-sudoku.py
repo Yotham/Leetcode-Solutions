2f4d7b5bcf795af98a9e27bcb411486ce8e4e1f1
Yotham <yothamsage@gmail.com>
2024-02-07 02:03:14 -0500

    Time: 57 ms (88.68%) | Memory: 11.8 MB (73.90%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -4,20 +4,41 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: bool
         """
-        rows = defaultdict(set)
-        columns = defaultdict(set)
-        box = defaultdict(set)
-        for r in range(len(board)):
-            for c in range(len(board)):
-                if board[r][c] == '.':
-                    continue
+        for i in range(len(board)):
+            column = []
+            row = []
+            for j in range(len(board)):
+                if board[j][i] != '.':
+                    row.append(board[j][i])
+                if board[i][j] != '.':
+                    column.append(board[i][j])
+            if len(row) != len(set(row)):
+                return False
+            if len(column) != len(set(column)):
+                return False
+
+        def subBox(values):
+            final = []
+            for num in values:
+                if num != '.':
+                    final.append(num)
+            if len(final) == len(set(final)):
+                return True
+            return False
+            
+        for i in range(len(board)):
+            for j in range(len(board)):
+                if i == 1 or i == 4 or i == 7:
+                    if j == 1 or j == 4 or j == 7:
+                        values = [board[i][j],board[i][j-1],board[i][j+1],
+                        board[i-1][j],board[i-1][j-1],board[i-1][j+1],
+                        board[i+1][j],board[i+1][j-1],board[i+1][j+1]]
+                        if not subBox(values):
+                            return False
                 else:
-                    if (board[r][c] in rows[r]) or (board[r][c] in columns[c]) or (board[r][c] in box[r//3,c//3]):
-                        return False
-                columns[c].add(board[r][c])
-                rows[r].add(board[r][c])
-                box[r//3,c//3].add(board[r][c])
-                
+                    continue
+        
+                        
 
         
         return True
